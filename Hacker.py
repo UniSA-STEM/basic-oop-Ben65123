6c02dab3860170c53ff04cacbf56ebd83abd646d
benjaminsienicki <sieby003@mymail.unisa.edu.au>
2025-10-17 15:51:21 +1030

    launch data spike and extract unsecured assests completed

--- Hacker.py
+++ Hacker.py
@@ -15,7 +15,10 @@ class Hacker:
         self.trace_level = 0
         self.exposed = False
 
+
+
     def acquire_a_rig(self, rig= None):
+        # Allows the hacker to acquire a rig. Costs one CryptoToken. The rig can be passed in or created here.
         if 'CryptoToken' in self.inventory:
             self.inventory.remove('CryptoToken')
             self.rig = rig
@@ -25,8 +28,68 @@ class Hacker:
             print('f{self.name} does not have enough CryptoTokens to acquire a rig')
 
     def increase_trace_level(self, amount):
+        #Increases trace level; exposes hacker if it exceeds threshold.
         self.trace_level += amount
         print(f"{self.name} trace level: {self.trace_level}")
+
+        if self.trace_level > 5:
+            self.exposed = True
+            print(f"{self.name} has been exposed.")
+
+    def decrease_trace_level(self, amount):
+        #Decreases trace level and hides hacker when it is safe again
+        if self.trace_level > 0:
+            self.trace_level -= amount
+            if self.trace_level < 0:
+                self.trace_level = 0
+            print(f"{self.name} trace level: {self.trace_level}")
+
+        if self.exposed == True and self.trace_level <= 5:
+            self.exposed = False
+            print(f"{self.name} has been hidden.")
+
+    def launch_data_spikes(self, target_rig):
+        # launches data spikes at other rigs.
+        if 'Data Spike' in self.rig.storage:
+            self.rig.storage.remove('Data Spike')
+            target_rig.damage += 1
+            print('f{self.name} has damaged {target_rig} with a Data Spike')
+        else:
+            print(f'{self.name} does not have enough Data Spike')
+
+    def extract_unsecured_assets(self, target_rig):
+
+        if not self.rig:
+            print(f"{self.name} has no rig to extract with.")
+            return
+
+        if target_rig.damage < target_rig.max_damage:
+            print(f"{target_rig.name} is not broken; extraction not allowed.")
+            return
+
+        if 'Removable Drive' not in self.rig.storage:
+            print(f"{self.name} does not have a Removable Drive.")
+            return
+
+        self.rig.storage.remove('Removable Drive')
+        print(f"{self.name} used a Removable Drive to extract assets from {target_rig.name}.")
+
+        for asset in list(target_rig.storage):
+            if not asset.encrypted:
+                self.inventory.append(asset)
+                target_rig.storage.remove(asset)
+
+        target_rig.storage.clear()
+
+        print(f"{self.name} extracted items: {self.inventory}")
+
+
+
+
+
+
+
+
 
 
 
