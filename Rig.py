4a4370c1d8d920ce83d208aa47a0c6caa0296c72
benjaminsienicki <sieby003@mymail.unisa.edu.au>
2025-10-19 13:16:32 +1030

    started and finished upgrade method for rig this time useing self.storage instead of counters

--- Rig.py
+++ Rig.py
@@ -17,29 +17,43 @@ class Rig:
         self.damage_counter = 0
         self.broken_state = False
         self.level = 0
+        # creates 2 data spikes and 1 removable drive
         self.storage = [
             Asset("Data Spike", "Used to attack other rigs"),
             Asset("Data Spike", "Used to attack other rigs"),
             Asset("Removable Drive", "Used to extract unsecured assets")]
 
 
-        # create two data spike objects
-        ds1 = Asset("ds1", "dataspike object")
-        ds2 = Asset("ds2", "dataspike object")
-        self.storage = [ds1, ds2]
 
-    def repair(self, crypto_token): #? what to do with crypto token
-        if not isinstance(crypto_token, int):
-            print('CryptoToken must be an int')
-        else:
-            if self.broken_state == False and self.damage_counter == 0:
-                print('No repair is needed!')
-            else:
-                self.damage_counter == 0
-                self.broken_state = False
+
+    def repair(self, crypto_token):
+        #repairs the rig using a cryptoToken if it is a damaged or broken.
+        if not isinstance(crypto_token, Asset) or crypto_token.name != 'CryptoToken':
+            print('A valid CryptoToken is required to repair the rig.')
+            return
+
+        if self.broken_state == False and self.damage_counter == 0:
+            print('No repair is needed!')
+            return
+        self.damage_counter = 0
+        self.broken_state = False
+        print(f'{self.name} has been repaired!')
 
     def upgrade(self, hardware_patch):
+        #upgrades the rig using a hardware patch from storage
+        patch = None
+        for asset in self.storage:
+            if asset.name == 'Hardware Patch':
+                patch = asset
+
+        if not patch:
+            print(f"{self.name} does not have a Hardware Patch to upgrade.")
+            return
+
+        self.storage.remove(patch)
         self.level += 1
+        print(f"{self.name} has been upgraded to level {self.level}.")
+
 
     def take_hit(self):
         self.damage_counter += 1
